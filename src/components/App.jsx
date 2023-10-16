4584dab698fc89b5aba5e7407fcae9a0c26035bd
Ariadna Ruiz <arsarbite@gmail.com>
2023-10-16 08:49:34 +0200

    clone repo

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+
+import reactLogo from '../images/react.svg'
+import '../styles/App.scss'
+
+function App() {
+ 
+
+  return (
+    <>
+   <header>
+    <h1>Country Info App</h1>
+   </header>
+   </>
+  )
+}
+
+export default App
