6366cbec7e926254ebe7f24d1af9302e36314373
Ariadna Ruiz <arsarbite@gmail.com>
2023-10-16 09:14:43 +0200

    initial exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,32 @@
 
+import { useState } from 'react'
 import reactLogo from '../images/react.svg'
 import '../styles/App.scss'
+import { useEffect } from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect (()=> {
+    fetch('https://restcountries.com/v3.1/all?fields=name,capital,flag,continents')
+    .then((response) => response.json())
+    .then ((data) =>{
+      setCountries(data)
+    })
+  },[])
  
+  const renderCountries = ()=> {
+    return countries.map((eachCountry, i) =>
+    (<li>
+      <p>Name: {eachCountry.name}</p>
+      <p>Capital:{eachCountry.capital}</p>
+      <p>Flag:</p>
+      <p>Continent:</p>
+    </li>
+
+    ))
+  }
 
   return (
     <>
